9b4a30af9d6c45e69431baad11b4015f23a98fd0
Rukkhadevata123 <144817389+Rukkhadevata123@users.noreply.github.com>
2024-09-23 19:09:14 +0800

    Update _null-ls.lua python

--- lua/_null-ls.lua
+++ lua/_null-ls.lua
@@ -1,10 +1,19 @@
 local null_ls = require("null-ls")
+local venv = os.getenv("VIRTUAL_ENV") or "~/.local/venv/nvim"
 
 null_ls.setup({
   sources = {
     null_ls.builtins.formatting.clang_format,      -- C/C++ 格式化
     null_ls.builtins.formatting.prettier,          -- JavaScript/TypeScript/HTML/CSS 格式化
-    -- null_ls.builtins.formatting.black,             -- Python 格式化
+    null_ls.builtins.formatting.black.with({
+      command = "bash",
+      args = {
+        "-c",
+        -- 先激活虚拟环境，运行 black 后，自动退出虚拟环境
+        "source ~/.local/venv/nvim/bin/activate && black --fast - && deactivate"
+      },
+      extra_args = { "--fast" },
+    }),             -- Python 格式化
     null_ls.builtins.diagnostics.flake8,           -- Python 诊断
     null_ls.builtins.diagnostics.eslint,           -- JavaScript 诊断
     null_ls.builtins.code_actions.gitsigns,        -- Git 符号操作
